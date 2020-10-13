07f14718a9c5bc21383525076c05c74bffc81a05
Olga Mazurina <olya_28_94@mail.ru>
2020-10-13 19:01:25 +0300

    init: srart Reverse Int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let s = n.toString();
+    let str = "";
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] == "-") {
+            continue;
+        }
+        str += s[i];
+    }
+    return +str;
+};
